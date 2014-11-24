c0adb234d8a55af0e21622e46568963444988a53
ilanasufrin <ilanasufrin@gmail.com>
2014-11-24 02:24:40 -0500

    just deleted some comments. turns out the code has been working the whole time, just not on emulators

--- main/java/com/example/android/sunshine/app/DetailActivity.java
+++ main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,21 +1,142 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
 package com.example.android.sunshine.app;
-
+ 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.VideoView;
-
-import java.io.IOException;
-
-
+ 
+ 
 public class DetailActivity extends ActionBarActivity {
-
+    private SurfaceHolder holder;
+    private SurfaceView mPreview;
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +146,19 @@ public class DetailActivity extends ActionBarActivity {
                     .add(R.id.container, new DetailFragment())
                     .commit();
         }
-
-
+ 
+ 
     }
-
-
+ 
+ 
+        //if I decide to add an options menu later this is where it'll go
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
 //        getMenuInflater().inflate(R.menu.menu_detail, menu);
 //        return true;
 //    }
-
+ 
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        // Handle action bar item clicks here. The action bar will
@@ -52,17 +174,23 @@ public class DetailActivity extends ActionBarActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-
+ 
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class DetailFragment extends Fragment {
-
+        public SurfaceHolder holder;
+      //  private SurfaceView mPreview;
+ 
         private static final String LOG_TAG = DetailFragment.class.getSimpleName();
-
+ 
         public DetailFragment() {
         }
-
+ 
+ 
+ 
+ 
+ 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -70,38 +198,43 @@ public class DetailActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.video_detail, container, false);
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
                 String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((VideoView) rootView.findViewById(R.id.detail_text))
-                        .setVideoPath(forecastStr);
-                //just a guess that the above line is correct
-                // }
-
-
+               VideoView mVideoView = ((VideoView) rootView.findViewById(R.id.detail_text));
+ 
+                mVideoView.setVideoPath(forecastStr);
+ 
+ 
+ 
                 String url = forecastStr; // your URL here
                 Log.v(LOG_TAG, "url for video:" + url);
-                MediaPlayer mediaPlayer = new MediaPlayer();
-
-//                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                try {
-                    mediaPlayer.setDataSource(url);
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "cannot find url for videos");
-                }
-                try {
-                    mediaPlayer.prepare(); // might take long! (for buffering, etc)
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "taking too long to play videos");
-                }
-                Log.v(LOG_TAG, "about to start media player");
-                mediaPlayer.start();
-
+//                MediaPlayer mediaPlayer = new MediaPlayer();
+//
+//
+////                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//                try {
+//                    mediaPlayer.setDataSource(url);
+//
+//                } catch (IOException e) {
+//                    Log.e(LOG_TAG, "cannot find url for videos");
+//                }
+//                try {
+//                    mediaPlayer.prepare(); // might take long! (for buffering, etc)
+//                } catch (IOException e) {
+//                    Log.e(LOG_TAG, "taking too long to play videos");
+//                }
+//                Log.v(LOG_TAG, "about to start media player");
+                //turns out I don't need a MediaPlayer because VideoView is a wrapper for it
+ 
+                mVideoView.requestFocus();
+                mVideoView.start();
+ 
             }
                 return rootView;
             }
-
-
+ 
+ 
     }
-
-
-
-
+ 
+ 
+ 
+ 
 }
--- main/java/com/example/android/sunshine/app/VideoFragment.java
+++ main/java/com/example/android/sunshine/app/VideoFragment.java
@@ -1,3 +1,354 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+160
+161
+162
+163
+164
+165
+166
+167
+168
+169
+170
+171
+172
+173
+174
+175
+176
+177
+178
+179
+180
+181
+182
+183
+184
+185
+186
+187
+188
+189
+190
+191
+192
+193
+194
+195
+196
+197
+198
+199
+200
+201
+202
+203
+204
+205
+206
+207
+208
+209
+210
+211
+212
+213
+214
+215
+216
+217
+218
+219
+220
+221
+222
+223
+224
+225
+226
+227
+228
+229
+230
+231
+232
+233
+234
+235
+236
+237
+238
+239
+240
+241
+242
+243
+244
+245
+246
+247
+248
+249
+250
+251
+252
+253
+254
+255
+256
+257
+258
+259
+260
+261
+262
+263
+264
+265
+266
+267
+268
+269
+270
+271
+272
+273
+274
+275
+276
+277
+278
+279
+280
+281
+282
+283
+284
+285
+286
+287
+288
+289
+290
+291
+292
+293
+294
+295
+296
+297
+298
+299
+300
+301
+302
+303
+304
+305
+306
+307
+308
+309
+310
+311
+312
+313
+314
+315
+316
+317
+318
+319
+320
+321
+322
+323
+324
+325
+326
+327
+328
+329
+330
+331
+332
+333
+334
+335
+336
+337
+338
+339
+340
+341
+342
+343
+344
+345
+346
+347
+348
+349
+350
+351
 /*
  * Copyright (C) 2014 The Android Open Source Project
  *
@@ -14,7 +365,7 @@
  * limitations under the License.
  */
 package com.example.android.sunshine.app;
-
+ 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -35,13 +386,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+ 
 import com.squareup.picasso.Picasso;
-
+ 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+ 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -51,30 +403,30 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-
+ 
+ 
 /**
  * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
  */
 public class VideoFragment extends Fragment {
-
+ 
     private VideoAdapter videoAdapter;
-
+ 
     public VideoFragment() {
     }
-
+ 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add this line in order for this fragment to handle menu events.
         setHasOptionsMenu(true);
     }
-
+ 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.videofragment, menu);
     }
-
+ 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -88,120 +440,114 @@ public class VideoFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
+ 
+ 
     private void updateVideos() {
         FetchVideoTask myFetchVideoTask = new FetchVideoTask();
-//       myFetchWeatherTask.execute("10003"); this hardcodes a string instead of getting it from preferences
+ 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        //once again, if I decide to add options, this is where it'll go
         //the second argument allows for a default location if the user hasn't set one
 //        String location = prefs.getString(getString(R.string.pref_location_key),  getString(R.string.pref_location_default));
 //        myFetchVideoTask.execute(location);
         myFetchVideoTask.execute();
     }
-
+ 
     //happens every time the fragment starts
     @Override
     public void onStart() {
         super.onStart();
         updateVideos();
     }
-
+ 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+ 
         final String LOG_TAG = FetchVideoTask.class.getSimpleName();
-
+ 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
         String[] data = {
                 "Welcome! Loading your videos"
         };
         List<String> videoresponses = new ArrayList<String>(Arrays.asList(data));
-
+ 
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
         // use it to populate the ListView it's attached to.
         videoAdapter = new VideoAdapter(
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_video);
-
+ 
 //        videoAdapter.add("http://i.imgur.com/DvpvklR.png");
-
+ 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         Log.v(LOG_TAG, "view has been inflated");
-
+ 
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_video);
         listView.setAdapter(videoAdapter);
-
-
-
-//        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
-        // Picasso.with(getActivity()).load(forecast).into(imageView);
-//        Picasso.with(imageView.getContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
-
-
-
+ 
+ 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+ 
             //this is where we're creating the detailed list item views
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-
-
+ 
+ 
                 String forecast = videoAdapter.getItem(position).getmVideoUrl();
-
-
-//                String toastMessage = new String("taking you to the video");
-//                Toast.makeText(getActivity(), toastMessage, Toast.LENGTH_SHORT).show();
-
+ 
+ 
+               String toastMessage = new String("taking you to the video");
+                Toast.makeText(getActivity(), toastMessage, Toast.LENGTH_SHORT).show();
+ 
                 Intent myIntent = new Intent(getActivity(), DetailActivity.class)
                         .putExtra(Intent.EXTRA_TEXT, forecast);
                 startActivity(myIntent);
-
+ 
             }
-
+ 
         });
-
+ 
         updateVideos();
-
+ 
         return rootView;
     }
-
+ 
     public class FetchVideoTask extends AsyncTask<String, Void, ArrayList<VideoObject>> {
-
+ 
         private final String LOG_TAG = FetchVideoTask.class.getSimpleName();
-
+ 
         @Override
         protected ArrayList<VideoObject> doInBackground(String... params) {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
-
+ 
             // Will contain the raw JSON response as a string.
             String videoJsonStr = null;
-
-
-
+ 
+ 
+ 
             try {
                 // Construct the URL for the  query
                 final String FORECAST_BASE_URL = "https://vine.co/api/timelines/users/918753190470619136";
-
-
+ 
+ 
                 Uri builtURI = Uri.parse(FORECAST_BASE_URL).buildUpon()
-
+ 
                         .build();
-
+ 
                 URL url = new URL(builtURI.toString());
                 Log.v(LOG_TAG, "Built URI: " + builtURI.toString());
-
+ 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
-
+ 
                 // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuffer buffer = new StringBuffer();
@@ -210,7 +556,7 @@ public class VideoFragment extends Fragment {
                     return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
-
+ 
                 String line;
                 while ((line = reader.readLine()) != null) {
                     // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
@@ -218,7 +564,7 @@ public class VideoFragment extends Fragment {
                     // buffer for debugging.
                     buffer.append(line + "\n");
                 }
-
+ 
                 if (buffer.length() == 0) {
                     // Stream was empty.  No point in parsing.
                     return null;
@@ -249,7 +595,7 @@ public class VideoFragment extends Fragment {
             }
             return null;
         }
-
+ 
         /**
          * Take the String representing the complete results in JSON Format and
          * pull out the data we need to construct the Strings needed for the wireframes.
@@ -259,78 +605,78 @@ public class VideoFragment extends Fragment {
          */
         private ArrayList<VideoObject>  getVideoDataFromJson(String forecastJsonStr)
                 throws JSONException {
-
+ 
             // These are the names of the JSON objects that need to be extracted.
             final String OWM_DATA = "data";
-
-
+ 
+ 
             JSONObject forecastJson = new JSONObject(forecastJsonStr);
             JSONObject weatherArray = forecastJson.getJSONObject(OWM_DATA);
-
+ 
             //we know can pass in string. but what about objects?
             ArrayList<VideoObject> resultStrs = new ArrayList<VideoObject>();
             for(int i = 0; i < weatherArray.length(); i++) {
-
+ 
                 String thumbnail;
                 JSONObject likesObject;
                 JSONArray records;
-
-
+ 
+ 
                 // Get the JSON object representing all the records
                 JSONArray dayForecast = weatherArray.getJSONArray("records");
-
-
+ 
+ 
                 //now we iterate over that string, looking for 'likes' objects
                 for (int j = 0; j < dayForecast.length(); j ++) {
                     likesObject = dayForecast.getJSONObject(j);
-
+ 
 //
-
+ 
                     //there only seem to be about 10 unique results
 //                    resultStrs.add(likesObject.getString("thumbnailUrl") + " posted by " + likesObject.getString("username"));
-
+ 
                     VideoObject myVideoObject = new VideoObject(likesObject.getString("thumbnailUrl"), likesObject.getString("username"), likesObject.getString("videoLowURL"));
                     resultStrs.add(myVideoObject);
-
+ 
 //                   for (int k = 0; k < likesObject.length(); k ++) {
 ////                       resultStrs.add(likesObject.getString("thumbnailUrl"));
 ////                       resultStrs.add(likesObject.getString("username"));
 //                   }
                 }
-
+ 
             }
             for (int i = 0; i <resultStrs.size(); i++) {
-
+ 
                     Log.v(LOG_TAG, "video array: " + resultStrs.get(i));
-
+ 
             }
-
+ 
             return resultStrs;
         }
-
+ 
         @Override
         protected void onPostExecute(ArrayList<VideoObject> result) {
             if (result != null) {
                 videoAdapter.clear();
                 for (VideoObject dayForecastString : result) {
-
+ 
                     videoAdapter.add(dayForecastString);
                 }
                 videoAdapter.notifyDataSetChanged();
             }
         }
-
-
+ 
+ 
     }
-
+ 
     private class VideoAdapter extends ArrayAdapter<VideoObject> {
-
+ 
         //can later change to type object
-
+ 
         public VideoAdapter(Context context, int resource ) {
             super(context, resource);
         }
-
+ 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater) getContext()
@@ -339,17 +685,18 @@ public class VideoFragment extends Fragment {
             View rowView = inflater.inflate(R.layout.list_item_video, parent, false);
             TextView textView = (TextView) rowView.findViewById(R.id.list_item_video_textview);
             textView.setText("Posted by " + videothumbnailstring.getmName());
-
+ 
             ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView);
-
-
+ 
+ 
             Picasso.with(getActivity()).load(videothumbnailstring.getmThumbnailUrl()).resize(200, 200).centerCrop().into(imageView);
+           //used for testing that the library works
 //        Picasso.with(imageView.getContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
-
+ 
             return rowView;
         }
     }
-
-
-
+ 
+ 
+ 
 }
